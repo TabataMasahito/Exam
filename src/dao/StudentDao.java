8d66a5c991e53d09d3d678ff37c517e199c22037
oohara <oohara@172.16.31.98>
2024-04-12 15:17:12 +0900

    4/12

--- src/dao/StudentDao.java
+++ src/dao/StudentDao.java
@@ -1,6 +1,14 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.School;
+import bean.Student;
 public class StudentDao extends Dao{
 
 	private String baseSql;
@@ -12,10 +20,10 @@ public class StudentDao extends Dao{
 
 	}
 
-	public List<Stuent> filter(School school, int entYear,String classNum,boolean isAttend) throws Exception{
+	public List<Student> filter(School school, int entYear,String classNum,boolean isAttend) throws Exception{
 
 	}
-	public List<Student> fillter<School school, int entYear,boolean isAttend) throws Exception{
+	public List<Student> fillter(School school, int entYear,boolean isAttend) throws Exception{
 
 	}
 
@@ -24,4 +32,47 @@ public class StudentDao extends Dao{
 
 	public boolean save(Student student)throws Exception{
 	}
+
+	private String baseSql1 ="select * from student where school_cd=?";
+
+
+	private List<Student>posFilter(ResultSet rSet,School school)throws Exception{
+		//リストを初期化
+		List<Student> list = new ArrayList<>();
+		try{
+			//リザルトリセットを全権走査
+			while(rSet.next()){
+				//学生インスタンスを初期化
+				Student student = new Student();
+				//学生インスタンスに検索結果をセット
+				student.setNo(rSet.getString("no"));
+				student.setName(rSet.getString("name"));
+				student.setEntYear(rSet.getInt("entYear"));
+				student.setClassNum(rSet.getString("class_num"));
+				student.setAttend(rSet.getBoolean("is_attend"));
+				student.setSchool(school);
+				//リストに追加
+				list.add(student);
+			}
+		}catch (SQLException | NullPointerException e){
+			e.printStackTrace();
+		}
+		return list;
+
+	}
+	public List<Student> filter(School school,int entYear, String classNum, boolean isAttend)throws Exception{
+		//リストを初期化
+		List<Student>list = new ArrayList<>();
+		//コネクションを確率
+		Connection connection = getConnection();
+		//プリペアードステートメント
+		PreparedStatement statement = null;
+		//リザルトセット
+		ResultSet rSet = null;
+		//SQL文の条件
+		String condition = "and ent_year=? and class_num=?";
+		//SQL文のソート
+
+	}
+
 }
