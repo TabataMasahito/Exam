5ad9b516b0cbbb59eac2d46b72b92ff8350ac842
oohara <oohara@172.16.31.113>
2024-05-16 14:53:03 +0900

    5/16です

--- src/scoremanager/main/SubjectUpdateExecuteAction.java
+++ src/scoremanager/main/SubjectUpdateExecuteAction.java
@@ -1,5 +1,8 @@
 package scoremanager.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -15,6 +18,7 @@ public class SubjectUpdateExecuteAction extends Action {
 		// TODO 自動生成されたメソッド・スタブ
 		HttpSession session = request.getSession();//セッション
 		Teacher teacher = (Teacher)session.getAttribute("user");
+		Map<String, String> errors = new HashMap<>();// エラーメッセージ
 
 
 		String cd=request.getParameter("cd");
@@ -27,13 +31,22 @@ public class SubjectUpdateExecuteAction extends Action {
 
 		Subject p=new Subject();
 
+
 		p.setCd(cd);
 		p.setName(name);
 
 
 
 		SubjectDao dao=new SubjectDao();
-		dao.save(p);
+		Subject subject = null;//学生
+		subject = dao.get(cd, teacher.getSchool());
+
+		if (subject == null) {
+			dao.save(p);
+		}
+		else{
+			errors.put("cd", "科目が存在していません");
+	}
 
 		request.getRequestDispatcher("subject_update_done.jsp").forward(request, response);
 	}
